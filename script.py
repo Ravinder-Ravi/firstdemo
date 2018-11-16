ff59f230fd852d78cdb6c7966127bfea708bf547
Ravinder-Ravi <ravinder0171@yahoo.com>
2018-11-17 12:59:57 +1300

    new123

---
+++ script.py
@@ -0,0 +1,12 @@
+import cv2
+img=cv2.imread("galaxy.jpg" , 0)
+print (type(img))
+print (img)
+print(img.shape)
+print(img.ndim)
+print ("Hello")
+
+resized_image=cv2.resize(img, (1000,500))
+cv2.imshow("Galaxy" ,resized_image)
+cv2.waitKey(2000)
+cv2.destroyAllWindows()
